5cbf7efdd761e384f573511ab1ae75baca829632
Oizon <william.galinat@gmail.com>
2022-12-09 22:43:00 -0500

    Tried to add bind variable functions - failed

--- force-app/main/default/lwc/certificationCalculator/certificationCalculator.js
+++ force-app/main/default/lwc/certificationCalculator/certificationCalculator.js
@@ -1,6 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class CertificationCalculator extends LightningElement {}
+export default class CertificationCalculator extends LightningElement {
+     
+}
 
 // Storage of Variables
 
@@ -21,7 +23,21 @@ let testingDebuggingAndDeployment;
 
 
 //Footer Change
-//If total > passingScore Green
-//else if total < passingScore Red
+//If total > passingScore Green  class="slds-text-color_success"
+//else if total < passingScore Red  class="slds-text-color_error"
 //else no formatting
 
+
+//inputing the entire if else if statement into 
+//the bind variable instead of caculating?
+/*finalScore = 65;
+
+passFail (){
+    if(finalScore >= 68){
+        return "Congrats you passed!";
+    }else if(finalScore < 68){
+        return 'You will get it next time!';
+    }else{
+        return 'Awaiting results...';
+    }
+}*/   
